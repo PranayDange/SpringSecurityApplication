74266a05d1579aec05a08177da98ed08b52054ba
PranayDange <pranaydange04@gmail.com>
2026-02-25 09:21:35 +0530

    Demo Application Initial commit

---
+++ src/main/java/com/coding/SecurityApp/SecurityApplication/SecurityApplication.java
@@ -0,0 +1,14 @@
+package com.coding.SecurityApp.SecurityApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+    //Using generated security password: 9ed3e8a1-122c-48e7-b5de-d5356f5968cb
+}
