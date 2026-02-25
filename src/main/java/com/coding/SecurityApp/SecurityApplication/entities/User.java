24a082c36ea0eeb4d8516743a941f2f89c6413d0
PranayDange <pranaydange04@gmail.com>
2026-02-25 09:29:36 +0530

    User entity implements UserDetails

--- src/main/java/com/coding/SecurityApp/SecurityApplication/entities/User.java
+++ src/main/java/com/coding/SecurityApp/SecurityApplication/entities/User.java
@@ -1,9 +1,15 @@
 package com.coding.SecurityApp.SecurityApplication.entities;
 
 import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +18,19 @@ public class User {
     @Column(unique = true)
     private String email;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
