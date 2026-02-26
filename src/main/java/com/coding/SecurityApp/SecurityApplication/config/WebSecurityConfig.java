daa1f65991517319fb8d86b23bcd61c12fa3c31f
PranayDange <pranaydange04@gmail.com>
2026-02-26 09:37:11 +0530

    request matchers,csrf and session management

---
+++ src/main/java/com/coding/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.coding.SecurityApp.SecurityApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/posts", "/error", "public/**").permitAll()//it can be used for whitelisting certain api(making it public)
+                        .requestMatchers("/posts/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig -> sessionConfig
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //.formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService myInMemoryUserDetailsService() {
+        UserDetails normalUser = User
+                .withUsername("pran")
+                .password(passwordEncoder().encode("Pran123"))
+                .roles("User")
+                .build();
+        UserDetails adminUser = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+        //InMemoryUserDetailsManager what does this mean
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
